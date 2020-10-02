2a89c7d3f222c31a06c80ad5efbbd3ddd458eac0
EnochEronmwonsuyiOPHS <71496939+EnochEronmwonsuyiOPHS@users.noreply.github.com>
2020-10-02 12:31:28 +0100

    Create gameintro.py

---
+++ gameintro.py
@@ -0,0 +1,13 @@
+# Hint, use variables, input and selection
+# Goodluck!
+print("Welcome to the Game, please select an option")
+
+age = int(input("Enter your age"))
+
+gender = str(input("Are you a male or a female"))
+
+email_address = str(input("Please enter an email address"))
+
+player_name = str(input("Enter your player's name"))
+
+print(" Are the following details correct: age =",age, "gender =",gender, "email address=",email_address, "player name =" , player_name, "yes or no?"))

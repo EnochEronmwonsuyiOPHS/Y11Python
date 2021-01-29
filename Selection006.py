20a9fe26a24ea93ea2ae8e28d33240591bb9e921
EnochEronmwonsuyiOPHS <71496939+EnochEronmwonsuyiOPHS@users.noreply.github.com>
2021-01-29 10:39:37 +0000

    Create Selection006.py

---
+++ Selection006.py
@@ -0,0 +1,9 @@
+age = int(input("Enter your age"))
+if age >= 18:
+    print("You can vote")
+if age == 17:
+    print("You can learn how to drive")
+if age == 16:
+    print("You can buy a lottery ticket")
+if age < 16:
+    print("You can go trick or treating")

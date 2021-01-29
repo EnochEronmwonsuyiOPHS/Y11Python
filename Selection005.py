e331ab48ad0181404abe7961de632d8ede43c2c3
EnochEronmwonsuyiOPHS <71496939+EnochEronmwonsuyiOPHS@users.noreply.github.com>
2021-01-29 10:33:41 +0000

    Create Selection005.py

---
+++ Selection005.py
@@ -0,0 +1,9 @@
+raining = str(input("Is it raining now")).lower()
+if raining == "yes":
+    windy = str(input("Is it windy today"))
+    if windy == "yes":
+        print("It is too windy for an umbrella")
+    else:
+            print("take an umbrella")
+else:
+    print("Enjoy your day")

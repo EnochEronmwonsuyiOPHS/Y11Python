fb81666287298dfc00a7fcdd10358f0c03ac0b01
EnochEronmwonsuyiOPHS <71496939+EnochEronmwonsuyiOPHS@users.noreply.github.com>
2021-02-05 12:13:18 +0000

    Create WhileLoopChallanges03.py

---
+++ WhileLoopChallanges03.py
@@ -0,0 +1,14 @@
+num = int(input("Enter a number"))
+firstnum = num
+total = firstnum
+looping = "y"
+
+while num < 50:
+    num2 = int(input("Enter another number"))
+    total = firstnum + num2
+    question = str(input("Do you want to add another    number y or n"))
+    if question == "y":
+        num3 = int(input("Enter a number"))
+        total = total ++ num3
+    if question == "n":
+        print("The total is",total)

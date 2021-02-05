a7d881d2958d9a08cbd8059da1910fa425090390
EnochEronmwonsuyiOPHS <71496939+EnochEronmwonsuyiOPHS@users.noreply.github.com>
2021-02-05 12:27:31 +0000

    Create WhileLoopChallanges04.py

---
+++ WhileLoopChallanges04.py
@@ -0,0 +1,12 @@
+count = 0
+while count < 5:
+    guest1 = str(input("Enter the name of guest"))
+    print(guest1, "has now been invited to the party")
+    count = count + 1
+    q = str(input("Do you want to invite more people y or n"))
+    if q == "y":
+        guest2 = str(input("Enter the name of guest"))
+        print(guest2, "has now been invited to the party")
+        count = count + 1
+    if q == "n":
+        print(count, "guests have been invited")

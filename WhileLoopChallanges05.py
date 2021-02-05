9d1b1e65f32479aab102f74ae99ce0b66f8c8cf8
EnochEronmwonsuyiOPHS <71496939+EnochEronmwonsuyiOPHS@users.noreply.github.com>
2021-02-05 12:43:18 +0000

    Create WhileLoopChallanges05.py

---
+++ WhileLoopChallanges05.py
@@ -0,0 +1,16 @@
+compnum = 50
+guess = int(input("Guess the competition number"))
+count = 1
+while guess != compnum:
+    if guess < compnum:
+        print("You guess was too low")
+        guess = int(input("Guess the competition number"))
+        count = count + 1
+        
+    if guess > compnum:
+        print("Your guess was too high")
+        guess = int(input("Guess the competition number"))
+        count = count + 1
+    
+    if guess == compnum:
+        print("Well done you guessed the number")

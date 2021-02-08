7e878bf2bdbaacf699a5029756bc6ad9f929fb40
EnochEronmwonsuyiOPHS <71496939+EnochEronmwonsuyiOPHS@users.noreply.github.com>
2021-02-08 14:46:22 +0000

    Create WhileLoopChallanges06.py

---
+++ WhileLoopChallanges06.py
@@ -0,0 +1,13 @@
+num = int(input("Enter a number between 10 and 20"))
+if num < 10:
+    print("Too low")
+    print("")
+    print("Please try again")
+    num = int(input("Enter a number between 10 and 20"))
+elif num > 20:
+    print("Too high")
+    print("")
+    print("Please try again")
+    num = int(input("Enter a number between 10 and 20"))
+elif num > 10 and num < 20:
+    print("Thank you")

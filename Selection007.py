97bdb32fb51cea8d0836ab75c1d1363114fd6cea
EnochEronmwonsuyiOPHS <71496939+EnochEronmwonsuyiOPHS@users.noreply.github.com>
2021-02-08 14:09:52 +0000

    Create Selection007.py

---
+++ Selection007.py
@@ -0,0 +1,9 @@
+number = int(input("Enter a number"))
+if number < 10:
+    print("Too Low")
+if number > 10 and number < 20:
+    print("Correct")
+    
+else:
+    print("Too high you crazy pie")
+   

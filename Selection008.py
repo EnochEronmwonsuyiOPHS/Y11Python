58c9620157cfd007a87900e438094318b8f72312
EnochEronmwonsuyiOPHS <71496939+EnochEronmwonsuyiOPHS@users.noreply.github.com>
2021-02-08 14:28:16 +0000

    Create Selection008.py

---
+++ Selection008.py
@@ -0,0 +1,9 @@
+num = int(input("Enter a number 1,2 or 3"))
+if num == 1:
+    print("Thank you")
+elif num == 2:
+    print("Well done, your so smart")
+elif num == 3:
+    print("Amazing your are so good at this")
+else:
+    print("Really, what is wrong with you??")

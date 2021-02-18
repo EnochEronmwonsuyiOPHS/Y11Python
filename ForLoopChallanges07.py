322ce60e3e89e147b2ac12adc7ae124290f283ad
EnochEronmwonsuyiOPHS <71496939+EnochEronmwonsuyiOPHS@users.noreply.github.com>
2021-02-18 21:39:18 +0000

    Create ForLoopChallanges07.py

---
+++ ForLoopChallanges07.py
@@ -0,0 +1,9 @@
+total = 0
+for i in range(0,4):
+    num1 = int(input("Enter a number"))
+    q = str(input("Do you want that number to be included y or n"))
+    if q == "y":
+        total = total + num1
+    elif q == "n":
+        print("We will move on then")
+print("The total is", total)    

27619919529279a5c0973493f07e8882db230792
EnochEronmwonsuyiOPHS <71496939+EnochEronmwonsuyiOPHS@users.noreply.github.com>
2021-02-19 17:17:47 +0000

    Create StringSliceChallanges02.py

---
+++ StringSliceChallanges02.py
@@ -0,0 +1,10 @@
+first_name = str(input("Enter your firstname"))
+last_name = str(input("Enter your last name"))
+
+
+whole_name = first_name + last_name 
+
+length = len(whole_name)
+
+print(whole_name)
+print(length)

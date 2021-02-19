d5d3bbe5d68309deec944b496bd4e515ee380cf7
EnochEronmwonsuyiOPHS <71496939+EnochEronmwonsuyiOPHS@users.noreply.github.com>
2021-02-19 17:27:01 +0000

    Create StringSliceChallanges04.py

---
+++ StringSliceChallanges04.py
@@ -0,0 +1,12 @@
+ firstline = str(input("Enter the first line of a nursery rhyme"))
+
+length = len(firstline)
+
+print("This has", length, "letters in it")
+
+startnum = int(input("Enter a starting number"))
+endnum = int(input("Enetr an ending number"))
+
+part = firstline[startnum : endnum]
+ 
+print(part)

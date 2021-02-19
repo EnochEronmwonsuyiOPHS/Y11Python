82f167ee94c497fedd1f6de227cd9cb6c8326147
EnochEronmwonsuyiOPHS <71496939+EnochEronmwonsuyiOPHS@users.noreply.github.com>
2021-02-19 17:36:15 +0000

    Create StringSliceChallanges06.py

---
+++ StringSliceChallanges06.py
@@ -0,0 +1,10 @@
+firstname = str(input("Enter your first name"))
+
+if len(firstname) < 5:
+    lastname = str(input("Enter your last name"))
+    fullname = firstname + lastname
+    caps = firstname.upper()
+    print(caps)
+
+else:
+    print(firstname.lower())

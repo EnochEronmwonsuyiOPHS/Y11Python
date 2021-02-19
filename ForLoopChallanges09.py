617bb6f1110f10d3579ca3f698a73a9a8604e0a2
EnochEronmwonsuyiOPHS <71496939+EnochEronmwonsuyiOPHS@users.noreply.github.com>
2021-02-19 16:59:07 +0000

    Create ForLoopChallanges09.py

---
+++ ForLoopChallanges09.py
@@ -0,0 +1,9 @@
+num = int(input("How many people are you inviting to the party"))
+
+if num < 10:
+    for i in range(0,num):
+        nameofguest = str(input("What is the name of the guest"))
+        print(nameofguest, "has been invited")
+        
+if num >= 10:
+    print("Too many people")

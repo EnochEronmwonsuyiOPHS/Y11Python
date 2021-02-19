d4004dc92c76183107e48493bd73e409939dc09f
EnochEronmwonsuyiOPHS <71496939+EnochEronmwonsuyiOPHS@users.noreply.github.com>
2021-02-19 16:51:54 +0000

    Create ForLoopChallanges08.py

---
+++ ForLoopChallanges08.py
@@ -0,0 +1,13 @@
+q = str(input("Do you want to count up or down, u or d"))
+
+if q == "u":
+    topnumber = int(input("Enter the top number"))
+    for i in range(1,topnumber,+1):
+        print(i)
+
+elif q == "d":
+    downnumber = int(input("Enter a number below 20"))
+    for i in range(20,downnumber,-1):
+        print(i)
+else:
+    print("I don't understand")
